f47a3e07e283938bb59be3bd48c998ce4eb0e277
Andrew Archibald <andrew@aarchibald.com>
2022-03-19 18:28:18 -0600

    Fetch papers with script

---
+++ compilers/fetch_papers.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+wget -nc -O llvm/cgo04_lattner.pdf https://www.llvm.org/pubs/2004-01-30-CGO-LLVM.pdf
+wget -nc -O webassembly/pldi17_haas.pdf https://dl.acm.org/doi/pdf/10.1145/3062341.3062363
+wget -nc -O webassembly/atc19_jangda.pdf https://www.usenix.org/system/files/atc19-jangda.pdf
+wget -nc -O compcert.pdf https://xavierleroy.org/publi/compcert-CACM.pdf
