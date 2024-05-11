ca3e4e5c065c461bb719fb212671139144e44e53
Thalia Archibald <thalia@archibald.dev>
2024-05-11 13:13:57 -0700

    Remove incomplete fetch_papers.sh

--- compilers/fetch_papers.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-wget -nc -O llvm/cgo04_lattner.pdf https://www.llvm.org/pubs/2004-01-30-CGO-LLVM.pdf
-wget -nc -O webassembly/pldi17_haas.pdf https://dl.acm.org/doi/pdf/10.1145/3062341.3062363
-wget -nc -O webassembly/atc19_jangda.pdf https://www.usenix.org/system/files/atc19-jangda.pdf
-wget -nc -O compcert.pdf https://xavierleroy.org/publi/compcert-CACM.pdf
-wget -nc -O graalvm/futamura.pdf https://dl.acm.org/doi/pdf/10.1145/3359061.3361077
-wget -nc -O emscripten.pdf https://dl.acm.org/doi/pdf/10.1145/2048147.2048224
